7c5aee99d771b6e0f33773de733c94caa8406342
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-04-25 19:16:10 -0300

    Conclusão do requisito 11

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let botao = document.querySelector('button');
 let input = document.querySelector('input');
 let lista = document.querySelector('ol');
 let botaoLimpar = document.getElementById('apaga-tudo');
+let botaoFinalizado = document.getElementById('remover-finalizados');
 botaoLimpar.addEventListener('click', limpaLista);
+botaoFinalizado.addEventListener('click', limpaFinalizado);
 botao.addEventListener('click', adicionaTarefa);
 function adicionaTarefa() {
     let itemLista = document.createElement('li');
@@ -29,5 +31,13 @@ function limpaLista() {
     let tamanhoLista = lista.children.length;
     for (let index = 0; index < tamanhoLista; index += 1) {
         lista.removeChild(lista.children[0]);
+    }
+}
+function limpaFinalizado() {
+    let tamanhoLista = lista.children.length - 1;
+    for (let index = tamanhoLista; index > 0; index -= 1) {
+        if(lista.children[index].classList.contains('completed')) {
+            lista.removeChild(lista.children[index]);
+        } else {}
     }
 }
