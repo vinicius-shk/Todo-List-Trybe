c959cfbe8edb5461619186f602774c7d679c3304
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-04-25 19:01:14 -0300

    Conclusão do requisito 9

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ botao.addEventListener('click', adicionaTarefa);
 function adicionaTarefa() {
     let itemLista = document.createElement('li');
     itemLista.addEventListener('click', alteraCor);
+    itemLista.addEventListener('dblclick', alteraEstilo);
     itemLista.innerText = input.value;
     input.value = '';
     lista.appendChild(itemLista);
@@ -13,5 +14,12 @@ function alteraCor(evento) {
     for (let index = 0; index < lista.children.length; index += 1) {
         lista.children[index].classList.remove('selected');
     }
-    evento.target.className = 'selected';
+    evento.target.classList.add('selected');
+}
+function alteraEstilo(evento) {
+    if (evento.target.classList.contains('completed')) {
+        evento.target.classList.remove('completed');
+    } else {
+        evento.target.classList.add('completed');
+    }
 }
