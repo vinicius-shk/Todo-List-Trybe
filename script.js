12cf91ac49fba487b5d9b1eeb5fc0bec6e6cf51c
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-04-25 18:41:59 -0300

    Conclusão do requisito 7

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ let lista = document.querySelector('ol');
 botao.addEventListener('click', adicionaTarefa);
 function adicionaTarefa() {
     let itemLista = document.createElement('li');
+    itemLista.addEventListener('click', alteraCor);
     itemLista.innerText = input.value;
     input.value = '';
     lista.appendChild(itemLista);
+}
+function alteraCor(evento) {
+    evento.target.className = 'selected';
 }
